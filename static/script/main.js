e77e7ffb35ae9988c6b3297d45fb40845d34ee7d
ifthennextagency <ifthen.next.agency@gmail.com>
2025-12-04 22:36:02 +0100

    update version

--- static/script/main.js
+++ static/script/main.js
@@ -1,8 +1,86 @@
+function Header() {
+    return (
+        <>
+            <header>
+                <div className="title">App meteo</div>
+            </header>
+        </>
+    )
+}
+
+function Main() {
+    let [temp, setTemp] = React.useState("")
+    let [cit, setCit] = React.useState("")
+    let [day, setDay] = React.useState("")
+
+    React.useEffect(() => {
+        function geoLoc() {
+            console.log("Avviata")
+            navigator.geolocation.getCurrentPosition((pos) => {
+                try {
+                    let lat = pos.coords.latitude;
+                    let lon = pos.coords.longitude;
+
+                    meteo(lat, lon)
+                } catch(error) {    
+                    console.log(errore)
+                }
+            })
+        }
+
+        geoLoc()
+    }, [])
+
+    async function meteo(lat, lon) {
+        let url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`;
+        let url_citta = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}&localityLanguage=it`;
+
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            setTemp(data.current_weather.temperature)
+            if (data.current_weather.is_day == 0) {
+                setDay("Notte")
+            } else {
+                setDay("Giorno")
+            }
+        })
+
+        fetch(url_citta)
+        .then(response => response.json())
+        .then(data => {
+            setCit(data.city)
+        })
+    }
+
+    return (
+        <>
+            <main>
+                <div>{cit}</div>
+                <div>{temp}</div>
+                <div>{day}</div>
+            </main>
+        </>
+    )
+}
+
+function Footer() {
+    return (
+        <>
+            <footer>
+                FOOTER
+            </footer>
+        </>
+    )
+}
+
 function App() {
     return (
         <>
             <div className="container">
-                Ciao mondo
+                <Header />
+                <Main />
+                <Footer />
             </div>
         </>
     )
