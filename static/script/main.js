35015ca2d2c9cd56190eb73c9daf93feab20b4ec
ifthennextagency <ifthen.next.agency@gmail.com>
2025-12-04 21:58:54 +0100

    update version

---
+++ static/script/main.js
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <>
+            <div className="container">
+                Ciao mondo
+            </div>
+        </>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('app'))
+root.render(<App />)
